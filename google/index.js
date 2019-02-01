798a252737f49ad6be3674e5e98d358d0b175e41
Mark Edomwande <medomwande@gmail.com>
2019-02-01 21:37:24 +0100

    removed the temp data for testing

--- google/index.js
+++ google/index.js
@@ -13,34 +13,7 @@ app.controller('MainCtrl', function($scope) {
       "mobileNetworkCode": '',
       "signalStrength": ''
     }]
-  },{
-  "id": 2,
-  "cellTowers": [{
-    "cellId": 9341,
-    "locationAreaCode": 12710,
-    "mobileCountryCode": 255,
-    "mobileNetworkCode": 6,
-    "signalStrength": 28
-  }]
-},{
-  "id": 3,
-  "cellTowers": [{
-    "cellId": 9243,
-    "locationAreaCode": 12710,
-    "mobileCountryCode": 255,
-    "mobileNetworkCode": 6,
-    "signalStrength": 26
-  }]
-},{
-  "id": 4,
-  "cellTowers": [{
-    "cellId": 9401,
-    "locationAreaCode": 12710,
-    "mobileCountryCode": 255,
-    "mobileNetworkCode": 6,
-    "signalStrength": 17
-  }]
-}];
+  }];
 
   $scope.index = $scope.cells.length;
 
