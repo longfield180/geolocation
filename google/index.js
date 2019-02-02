2d6c80417a3dcab1292eec46b3647526ae67d2fe
Mark Edomwande <medomwande@gmail.com>
2019-02-02 14:00:56 +0100

    removed the temp data for testing

--- google/index.js
+++ google/index.js
@@ -365,30 +365,49 @@ function placeMarker(data, cell, waps, color) {
   const marker = new google.maps.Marker({
     position: position,
     map: map,
-    icon: {
-      url: "http://maps.google.com/mapfiles/ms/icons/"+color+"-dot.png"
-    }
+    // icon: {
+    //     path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
+    //     strokeColor: `${color}`,
+    //     scale: 3
+    // },
+    icon:{
+      path: 'm 12,2.4000002 c -2.7802903,0 -5.9650002,1.5099999 -5.9650002,5.8299998 0,1.74375 1.1549213,3.264465 2.3551945,4.025812 1.2002732,0.761348 2.4458987,0.763328 2.6273057,2.474813 L 12,24 12.9825,14.68 c 0.179732,-1.704939 1.425357,-1.665423 2.626049,-2.424188 C 16.809241,11.497047 17.965,9.94 17.965,8.23 17.965,3.9100001 14.78029,2.4000002 12,2.4000002 Z',
+      fillColor: `${color}`,
+      fillOpacity: 1.0,
+      strokeColor: '#000000',
+      strokeWeight: 1,
+      scale: 2,
+      anchor: new google.maps.Point(12, 24),
+    },
+    // icon: `assets/${color}.png`,
+    // icon: {
+    //   url: "http://maps.google.com/mapfiles/ms/icons/"+color+"-dot.png"
+    // }
   });
-
-  if(data.radius || data.accuracy) {
-    let radius = parseInt(data.radius) || parseInt(data.accuracy)
-    const circle = new google.maps.Circle({
-      map: map,
-      radius: radius,
-      fillColor: '#313131',
-      fillOpacity: .3,
-      strokeColor: '#fff',
-      strokeOpacity: .4,
-      strokeWeight: .8
-    });
-
-    circle.bindTo('center', marker, 'position');
-    gmarkers.push(circle);
-  }
 
   // Allow each marker to have an info window
   marker.addListener('click', function() {
+    if(data.radius || data.accuracy) {
+      let radius = parseInt(data.radius) || parseInt(data.accuracy)
+      const circle = new google.maps.Circle({
+        map: map,
+        radius: radius,
+        fillColor: '#313131',
+        fillOpacity: .0375,
+        strokeColor: '#fff',
+        strokeOpacity: .8,
+        strokeWeight: .8
+      });
+
+      circle.bindTo('center', marker, 'position');
+      gmarkers.push(circle);
+    }
+
     infowindow.open(map, marker);
+    if (map.getZoom() < 13) {
+      map.setZoom(13);
+      map.setCenter(marker.getPosition());
+    }
   });
 
   gmarkers.push(marker);
@@ -428,7 +447,7 @@ function triangulate(towers) {
   };
 
   placeMarker(data, null, null, 'red');
-  alert('Triagulations complete');
+  alert('Triagulation complete');
 }
 
 function numberParser(number) {
